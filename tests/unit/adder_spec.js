6f87af8ad79c958d5436feac623708884f2d1581
Chonlasith Jucksriporn <chonla@capabilify.com>
2016-11-03 17:25:27 +0700

    fix spec name and add functional test

--- tests/unit/adder_spec.js
+++ tests/unit/adder_spec.js
@@ -1,7 +1,13 @@
 describe("Adder", function() {
-    it("should be true", function() {
+    it("should return 3 when add by Adder", function() {
         var adder = new Adder(),
             result = adder.add(1, 2);
+
+        expect(result).toEqual(3);
+    });
+
+    it("should return 3 when add by add function", function() {
+        var result = add_number(1, 2);
 
         expect(result).toEqual(3);
     });
